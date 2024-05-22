6bd677cdaed07d2da25bc1c41af6e69361930a00
Piotr Karasinski <7767011+Caleb9@users.noreply.github.com>
2024-05-22 18:16:38 +0200

    Add godoc comments

--- add.go
+++ add.go
@@ -1,5 +1,8 @@
+// Package ex1 contains a solution to exercise 1
 package ex1
 
+// Add adds two numbers, according to rules of
+// https://www.mathsisfun.com/numbers/addition.html
 func Add(x, y int) int {
 	return x + y
 }
